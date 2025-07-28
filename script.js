5b5d0434ccb394d129d72ff3f3a5ab2a786004cd
Jdev01 <jose_segovia_01@hotmail.com>
2025-07-28 03:58:00 -0500

    feat: add carrusel and before/after effect

--- script.js
+++
@@ -1,7 +0,0 @@
-const slider = document.getElementById('slider');
-const afterContainer = document.querySelector('.after-images');
-
-slider.addEventListener('input', () => {
-  const percentage = slider.value;
-  afterContainer.style.width = `${percentage}%`;
-});
